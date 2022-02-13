9a5e8306ffa1d20dc635f8861c11754d87956fd6
SACHIT260 <96931281+SACHIT260@users.noreply.github.com>
2022-02-13 21:36:55 +0530

    Add files via upload

---
+++ w1ass1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+    char name[20];
+    int reg;
+    char branch[30];
+    char hobbies[50];
+    printf("Enter your name:\n");
+    gets(name);
+
+   
+    printf("Enter Branch:\n");
+    gets(branch);
+    printf("Enter Hobbies:\n");
+    gets(hobbies);
+    printf("Enter registration number:\n");
+    scanf("%d",& reg);
+    
+}
