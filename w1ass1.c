ae16b5d62d4c39c73e3f674192f5dc94d1ed586c
SACHIT260 <96931281+SACHIT260@users.noreply.github.com>
2022-02-13 21:35:53 +0530

    Delete w1ass1.c

--- w1ass1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-    printf("Name:Pulkit Sahoo\n");
-    printf("Registration number:067\n");
-    printf("Branch:Mechanical Engineering\n");
-    printf("Hobbies:To play chess\n");
-}
-
